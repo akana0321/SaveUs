e89fc1e810006b4b1c08710fa05ccfd7f3b39e6b
akana0321 <akana0321@gmail.com>
2021-04-04 16:01:56 +0900

    20210404 15:58 Yunse

--- app/src/main/java/com/example/saveus/FrameActivity.java
+++ app/src/main/java/com/example/saveus/FrameActivity.java
@@ -1,24 +1,32 @@
 package com.example.saveus;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class FrameActivity extends AppCompatActivity {
     Toolbar toolBar;
+    BottomNavigationView frBottom;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame);
 
+        // Toolbar 설정
         toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white);
+
+        // Bottom Navigation 설정
+        frBottom = (BottomNavigationView) findViewById(R.id.frBottom);
     }
 }
