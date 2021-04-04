d4ed123f6d1b41cc8c7bd5b1f236c92133c63f3e
akana0321 <akana0321@gmail.com>
2021-04-04 16:02:57 +0900

    20210404 15:58 Yunse

--- app/src/main/java/com/example/saveus/FrameActivity.java
+++ app/src/main/java/com/example/saveus/FrameActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class FrameActivity extends AppCompatActivity {
     Toolbar toolBar;
     BottomNavigationView frBottom;
+    String string;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
