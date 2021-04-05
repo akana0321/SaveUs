a6177bcdb1e0721ac967c34c63d0be0f253b8373
akana0321 <akana0321@gmail.com>
2021-04-05 15:41:52 +0900

    20210405 15:41

--- app/src/main/java/com/example/saveus/FrameActivity.java
+++ app/src/main/java/com/example/saveus/FrameActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 
@@ -28,6 +29,7 @@ public class FrameActivity extends AppCompatActivity {
     Toolbar toolBar;
     BottomNavigationView frBottom;
     FrameLayout content;
+    Button MainTv_EMER;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,13 +52,13 @@ public class FrameActivity extends AppCompatActivity {
         v1 = inflater.inflate(activity_main,null); //2. 메인 페이지를 뷰객체로 가져오기.
 
 
-        switch (v1.getId()){
-            case  R.id.mainTv_Emer : {
-            content = (FrameLayout) findViewById(R.id.content_layout);
-            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            inflater.inflate(activity_emergency, content, true);
-             }
-        }
+        MainTv_EMER = (Button) findViewById(R.id.mainTv_Emer);
+        MainTv_EMER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeView(0);
+            }
+        });
     }
 
 
@@ -71,9 +73,7 @@ public class FrameActivity extends AppCompatActivity {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
             case R.id.tbEmer :
-                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                inflater.inflate(activity_emergency,content,true); // *2021.04.05 응급처치 기능 클릭시, 본문이 프레임레이아웃 이므로 응급처치 페이지가 누적해서 보여줌.
-                                                                                // intent으로 해야 화면전환이 될 것 같음.
+                changeView(0);
                 return true;
                 // 응급처치 기능 클릭시 페이지 전환
             case R.id.tbAed:
@@ -90,6 +90,33 @@ public class FrameActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void changeView(int index) {
+        // LayoutInflater 초기화.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.content_layout);
+        if (frame.getChildCount() > 0) {
+            // FrameLayout에서 뷰 삭제.
+            frame.removeViewAt(0);
+        }
+
+        // XML에 작성된 레이아웃을 View 객체로 변환.
+        View view = null;
+        switch (index) {
+            case 0:
+                view = inflater.inflate(activity_emergency, frame, false);
+                break;
+            case 1:
+                view = inflater.inflate(activity_fracture, frame, false);
+                break;
+        }
+
+        // FrameLayout에 뷰 추가.
+        if (view != null) {
+            frame.addView(view);
         }
     }
 }
