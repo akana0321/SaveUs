861e7079e9b45e6e7d8e48f697f30968dc6144ee
akana0321 <akana0321@gmail.com>
2021-04-09 15:26:20 +0900

    20210409 15:25 EmergencyActivity - Youtube

--- app/src/main/java/com/example/saveus/EmergencyActivity.java
+++ app/src/main/java/com/example/saveus/EmergencyActivity.java
@@ -1,16 +1,99 @@
 package com.example.saveus;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class EmergencyActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class EmergencyActivity extends YouTubeBaseActivity {
+    // 객체 선언
+    YouTubePlayerView playerView;
+    YouTubePlayer player;
+
+    // 유튜브 API Key와 동영상 ID 변수설정
+    private String API_KEY = "AIzaSyCOO1gFLG45vDalw6gTYgcLI3AFw-Dm4gs";
+    private String videoId = "q3xPn7_AZTk";
+
+    // logcat 사용설정
+    private final String TAG = "EmergencyActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
         setTitle("응급처치");
+
+        initPlayer();
+
+        Button btnPlay = findViewById(R.id.youtubeBtn);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playVideo();
+            }
+        });
+    }
+
+    private void initPlayer() {
+        playerView = findViewById(R.id.youTubePlayerView);
+        playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                player = youTubePlayer;
+
+                player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+
+                    }
+
+                    @Override
+                    public void onLoaded(String id) {
+                        Log.d(TAG, "onLoaded : " + id);
+                        player.play();
+                    }
+
+                    @Override
+                    public void onAdStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoEnded() {
+
+                    }
+
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+                        Log.d(TAG, "onError : " + errorReason);
+                    }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+    }
+
+    private void playVideo() {
+        if (player != null) {
+            if (player.isPlaying())
+                player.pause();
+            player.cueVideo(videoId);
+        }
     }
 }
