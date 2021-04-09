a894809ed913a257cc9f1a82cc0da0d2442c97f6
akana0321 <akana0321@gmail.com>
2021-04-09 17:13:43 +0900

    20210409 17:11 emer->frac

--- app/src/main/java/com/example/saveus/EmergencyActivity.java
+++ app/src/main/java/com/example/saveus/EmergencyActivity.java
@@ -1,28 +1,26 @@
 package com.example.saveus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class EmergencyActivity extends YouTubeBaseActivity {
-    // 객체 선언
-    YouTubePlayerView playerView;
-    YouTubePlayer player;
-
-    // 유튜브 API Key와 동영상 ID 변수설정
-    private String API_KEY = "유튜브 API키 입력"; // 개인정보라서 깃허브에 올리면 위험함
-    private String videoId = "q3xPn7_AZTk";
-
-    // logcat 사용설정
-    private final String TAG = "EmergencyActivity";
+    TextView EmerTv_BruTv,EmerTv_BurnTv,EmerTv_AirTv,EmerTv_SeiTv,EmerTv_StiTv,Emer_HeaTv;
+    BottomNavigationView frBottom;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,74 +28,77 @@ public class EmergencyActivity extends YouTubeBaseActivity {
         setContentView(R.layout.activity_emergency);
         setTitle("응급처치");
 
-        initPlayer();   // 유튜브 플레이어 가져오기
+        EmerTv_BruTv = (TextView)findViewById(R.id.emerTv_bru);
+        EmerTv_BurnTv = (TextView)findViewById(R.id.emerTv_burn);
+        EmerTv_AirTv = (TextView)findViewById(R.id.emerTv_air);
+        EmerTv_SeiTv = (TextView)findViewById(R.id.emerTv_sei);
+        EmerTv_StiTv = (TextView)findViewById(R.id.emerTv_sti);
+        Emer_HeaTv = (TextView)findViewById(R.id.emerTv_heaTv);
 
-        Button btnPlay = findViewById(R.id.youtubeBtn);
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+        // Bottom Navigation 설정
+        frBottom = (BottomNavigationView)findViewById(R.id.frBottom_1);
+
+        EmerTv_BruTv.setOnClickListener(new View.OnClickListener() {  // 응급처치 클릭시 응급상황 분류 페이지 이동
             @Override
             public void onClick(View v) {
-                playVideo();    // 버튼 클릭시 동작
+                Intent intent = new Intent(getApplicationContext(), BruiseActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    // 유튜브 플레이어를 가져오는 메서드
-    private void initPlayer() {
-        playerView = findViewById(R.id.youTubePlayerView);
-        playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+        // 바텀 네이게이션 각 버튼 클릭시 실행.
+        frBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                player = youTubePlayer;
-                player.cueVideo(videoId);
-
-                player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
-                    @Override
-                    public void onLoading() {
-
-                    }
-
-                    @Override
-                    public void onLoaded(String id) {
-                        Log.d(TAG, "onLoaded : " + id); // 정상적으로 load가 되면 TAG에 상태 저장
-                    }
-
-                    @Override
-                    public void onAdStarted() {
-
-                    }
-
-                    @Override
-                    public void onVideoStarted() {
-
-                    }
-
-                    @Override
-                    public void onVideoEnded() {
-
-                    }
-
-                    @Override
-                    public void onError(YouTubePlayer.ErrorReason errorReason) {
-                        Log.d(TAG, "onError : " + errorReason);
-                        // 에러가 나면 에러 이유를 TAG에 상태 저장
-                    }
-                });
-
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.frBack :
+                        //Intent intent = new Intent(getApplicationContext(), .class);
+                        //startActivity(intent); // 2021.04.07 임시로 페이지가 전환되는 지 확인하기 위해 작성해 봄.
+                        return true;
+                    case R.id.frMain :
+                        return true;
+                    case R.id.frExit :
+                        moveTaskToBack(true); // 태스크를 백그라운드로 이동
+                        finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+                        System.exit(0);
+                        return true;
+                }
+                return false;
             }
         });
+
     }
 
-    // 버튼을 눌렀을 때 정지 상태에서는 재생, 재생 상태에서는 일시 정지
-    private void playVideo() {
-        if (player != null) {
-            if (player.isPlaying())
-                player.pause();
-            player.cueVideo(videoId);
+    public boolean onCreateOptionsMenu(Menu menu) { // 상단 우측 탭 호출
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.tbEmer :
+                Intent intent = new Intent(getApplicationContext(), EmergencyActivity.class);
+                startActivity(intent);
+                return true;
+            // 응급처치 기능 클릭시 페이지 전환
+            case R.id.tbAed:
+                //자동제세동기 기능 클릭시 페이지 전환
+                return true;
+            case R.id.tbMoun:
+                //등산중 사고 신고 클릭시 페이지 전환
+                return true;
+            case R.id.tbPati:
+                //환자 상태파악 클릭시 페이지 전환
+                return true;
+            case R.id.tbCont:
+                //문의하기 기능클릭시 페이지 전환
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
