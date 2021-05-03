ac48f3bafe45692179eeac39e25772e332f3ede7
gkswotjd45 <gkswotjd45@naver.com>
2021-05-03 20:56:32 +0900

    21.05.03 20:03 simple aed marker testing

--- app/src/main/java/com/example/saveus/AedPoint.java
+++ app/src/main/java/com/example/saveus/AedPoint.java
@@ -7,36 +7,36 @@ public class AedPoint {
     private double wgs84Lat; // 위도 값
     private String buildPlace; // 설치 대략적 위치
     private String buildAddress; // 설치 전체 주소
-    private String clerkㄴTel; // 설치기관 전화번호
+    private String clerkTel; // 설치기관 전화번호
 
     public AedPoint(){
         super();
     }
-
     public String getRnum(){ return  rnum; }
+    public String getOrg() {return org;}
     public double getWgs84Lat() {  // 위도 값 가져오기
         return wgs84Lat;
     }
-
+    public double getWgs84Lon() { // 경도 값 가져오기
+        return wgs84Lon;
+    }
     public String getBuildPlace() { // 설치장소 가져오기.
         return buildPlace;
     }
     public String getBuildAddress() { // 설치기관 주소 가져오기
         return buildAddress;
     }
-
     public String getClerkTel() {  // 설치기관 전화번호 가져오기
         return clerkTel;
     }
 
-    public double getWgs84Lon() { // 경도 값 가져오기
-        return wgs84Lon;
-    }
+
 
     public  void setRnum(String rnum){
         this.rnum = rnum;
     }
 
+    public  void setOrg(String org){this.org = org;}
     public void setWgs84Lon(double wgs84Lon) { // 경도 값 설정하기
         this.wgs84Lon = wgs84Lon;
     }
@@ -55,13 +55,6 @@ public class AedPoint {
 
     public void setClerkTel(String clerkTel) { // 설치기관 전화번호 가져오기
         this.clerkTel = clerkTel;
-    }
-
-    public String findIndex(String buildPlace){
-        if(this.buildPlace.equals(buildPlace)){
-            return rnum;
-        }
-        else  return  null;
     }
 
 }
